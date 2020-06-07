06ee54109076d860d934f8fa7103641a01097cad
Celal ORMANLI <ormanlicelal@gmail.com>
2020-06-07 21:28:25 +0300

    create project and add learn word activity

---
+++ app/src/main/java/com/celalormanli/wordmemorization/LearnWord.kt
@@ -0,0 +1,14 @@
+package com.celalormanli.wordmemorization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LearnWord : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn_word)
+        supportActionBar?.setBackgroundDrawable(resources.getDrawable(R.drawable.main_background))
+
+    }
+}
